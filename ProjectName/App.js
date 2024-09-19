5c3a4d63c34079571565b9b01ed90b478d78d43b
tinph010803 <tinphan924@gmail.com>
2024-09-19 11:15:45 +0700

    add second

--- ProjectName/App.js
+++ ProjectName/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your appzaa!</Text>
+      <Text>Open up App.js to start working on your appzaa!hellozxcxzc</Text>
       <StatusBar style="auto" />
     </View>
   );
